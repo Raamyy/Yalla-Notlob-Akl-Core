ee622c2fd11d812d86f29ebe206d9ce75640ca95
islammohsen <islammohsenmohamed.1998@gmail.com>
2021-02-07 07:43:10 +0200

    Added GetOrderReceipt

--- Business/OrderCalculations.cs
+++ Business/OrderCalculations.cs
@@ -5,6 +5,17 @@ using Yalla_Notlob_Akl.Models;
 
 namespace Yalla_Notlob_Akl.Business
 {
+    public class PersonOrderSummary{
+        public double totalPrice;
+        public double basePrice;
+        public double taxFees;
+        public double deleivryFees;
+        public List<OrderItem> orderItems;
+        public PersonOrderSummary(){
+            totalPrice = basePrice = taxFees = deleivryFees = 0;
+            orderItems = new List<OrderItem>();
+        }
+    }
     public static class OrderCalculations
     {
         public static Dictionary<Item, int> GetOrderSummary(List<OrderItem> orderItems)
@@ -23,6 +34,26 @@ namespace Yalla_Notlob_Akl.Business
                 dict[new ItemDao().Get(itemSummary.ItemId)] = itemSummary.ItemCount;
             }
             return dict;
+        }
+        public static Dictionary<Person, PersonOrderSummary> GetOrderReceipt(List<OrderItem> orderItems, double taxFees, double deleivryFees){
+            double basePrice = 0;
+            Dictionary<Person, PersonOrderSummary> personOrder = new Dictionary<Person, PersonOrderSummary>();
+            foreach (var orderItem in orderItems)
+            {
+                Item item = new ItemDao().Get(orderItem.ItemId);
+                double currentItemPrice = item.price.Value * orderItem.Quantity;
+                basePrice += currentItemPrice;
+                Person person = new PersonDao().Get(orderItem.PersonId);
+                personOrder[person].basePrice += currentItemPrice;
+                personOrder[person].orderItems.Add(orderItem);
+            }
+            foreach (var item in personOrder)
+            {
+                item.Value.taxFees = (item.Value.basePrice / basePrice) * taxFees;
+                item.Value.deleivryFees = (item.Value.basePrice / basePrice) * deleivryFees;
+                item.Value.totalPrice = item.Value.basePrice + item.Value.taxFees + item.Value.deleivryFees;
+            }
+            return personOrder;
         }
     }
     public class OrderStatsVM{
